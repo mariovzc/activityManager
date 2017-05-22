ac69aa5ee8f72ef665f974521096f58b7ef3c3ab
Mario Vizcaino <mario.vizcainos@gmail.com>
2017-05-22 15:41:08 -0500

    removed commnets

--- Activity manager/ModelHandler/Person+CoreDataClass.swift
+++ Activity manager/ModelHandler/Person+CoreDataClass.swift
@@ -1,11 +1,3 @@
-//
-//  Person+CoreDataClass.swift
-//  Activity manager
-//
-//  Created by Giancarlo Valencia on 5/22/17.
-//  Copyright © 2017 None. All rights reserved.
-//
-
 import Foundation
 import CoreData
 
