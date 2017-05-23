4a070576ddbc39dd73351e053cd161082adab03c
Mario Vizcaino <mario.vizcainos@gmail.com>
2017-05-23 09:42:05 -0500

    created the new views for organizations

---
+++ Activity manager/Controllers/NewOrganizationVC.swift
@@ -0,0 +1,30 @@
+//
+//  NewOrganizationVC.swift
+//  Activity manager
+//
+//  Created by Giancarlo Valencia on 5/23/17.
+//  Copyright © 2017 None. All rights reserved.
+//
+
+import UIKit
+
+class NewOrganizationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backButtonAction(_ sender: Any) {
+    }
+
+    @IBAction func saveOrganization(_ sender: Any) {
+    }
+
+}
