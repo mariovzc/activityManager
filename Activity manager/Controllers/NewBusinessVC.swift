1bf4809bf4552e327ff54a5c60d3f8216abee96c
Mario Vizcaino <mario.vizcainos@gmail.com>
2017-05-23 17:40:04 -0500

    set the pickers initial values

--- Activity manager/Controllers/NewBusinessVC.swift
+++ Activity manager/Controllers/NewBusinessVC.swift
@@ -1,11 +1,3 @@
-//
-//  NewBusinessVC.swift
-//  Activity manager
-//
-//  Created by Giancarlo Valencia on 5/23/17.
-//  Copyright © 2017 None. All rights reserved.
-//
-
 import UIKit
 
 class NewBusinessVC: UIViewController {
@@ -26,10 +18,24 @@ class NewBusinessVC: UIViewController {
     
     @IBOutlet weak var datePicker: UIDatePicker!
     
+    var date: Date!
+
+    let managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    var personService: PersonService!
+    var people : [Person] = []
+    var selectedPerson = 0
+    
+    var organizationService: OrganizationService!
+    var organizations : [Organization] = []
+    var selectedOrganization = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        initialData()
+        date = datePicker.date
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,9 +44,71 @@ class NewBusinessVC: UIViewController {
     }
     
     @IBAction func backAction(_ sender: UIBarButtonItem) {
+        back()
     }
 
     @IBAction func saveAction(_ sender: Any) {
     }
 
+}
+
+extension NewBusinessVC: UIPickerViewDelegate, UIPickerViewDataSource{
+    func initialData() {
+        datePicker.addTarget(self, action: #selector(dateChanged(_:)), for: .valueChanged)
+        date = datePicker.date
+
+        personService = PersonService(context: managedContext)
+        people = personService.getAll()
+        personTextField.text = people.first?.name
+
+        organizationService = OrganizationService(context: managedContext)
+        organizations = organizationService.getAll()
+        organizationTextField.text = organizations.first?.name
+        
+        hidePickers()
+    }
+    func back(){
+        navigationController?.popViewController(animated: true)
+        dismiss(animated: true, completion:nil)
+    }
+    //this get the datepicker value
+    func dateChanged(_ sender: UIDatePicker) {
+        date = datePicker.date
+        print(datePicker.date)
+    }
+    func hidePickers(){
+        personPicker.isHidden = true
+        organizationPicker.isHidden = true
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == personPicker{
+            return people.count
+        }else{
+            return organizations.count
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView == personPicker{
+            return people[row].name
+        }else{
+            return organizations[row].name
+        }
+
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView == personPicker{
+            personTextField.text = people[row].name
+            selectedPerson = row
+        }else{
+            organizationTextField.text = organizations[row].name
+            selectedOrganization = row
+        }
+
+    }
 }
